b8e83f4f51d2968c10ac0603a0add8a04710de09
Sebastian Wendt <wendseb90@gmail.com>
2019-11-03 17:12:35 +0100

    First commit Pi_onier

---
+++ Pi_onier.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# Pi_onier is a simple script to return the value of Pi with a set number of decimals using the Cudnovsky Algorithm
+
+from decimal import Decimal, getcontext
+import math
+import time
+import colorama
+from colorama import Fore
+
+colorama.init()
+
+numberofdigits = int(input("Bitte geben Sie die Anzahl der Dezimalstellen für PI an: "))
+getcontext().prec = numberofdigits
+start_time = time.time()
+
+
+def calc(n):
+    t = Decimal(0)
+    pi = Decimal(0)
+    deno = Decimal(0)
+    k = 0
+    for k in range(n):  # Copy_paste Algorithmus
+        t = (Decimal(-1) ** k) * (math.factorial(Decimal(6) * k)) * (13591409 + 545140134 * k)
+        deno = math.factorial(3 * k) * (math.factorial(k) ** Decimal(3)) * (640320 ** (3 * k))
+        pi += Decimal(t) / Decimal(deno)
+    pi = pi * Decimal(12) / Decimal(640320 ** Decimal(1.5))
+    pi = 1 / pi
+    return str(pi)
+
+
+print(calc(1))
+print(Fore.RED + "\nTime taken:", time.time() - start_time)
